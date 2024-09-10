9b6f6d553f78777262c2d776b7ef38fe8f1714ed
lllinginging <llling10099@gmail.com>
2024-09-09 23:30:34 -0700

    update cart quantity

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,16 +1,23 @@
-import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
+import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, TextField, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/product";
-import axios from "axios";
 import agent from "../../app/api/agent";
 import NotFound from "../../app/errors/ServerError";
 import Spinner from "../../app/layout/Spinner";
+import { useAppSelector, useAppDispatch } from "../../app/store/configureStore";
+import { LoadingButton } from "@mui/lab";
+
 
 export default function ProductDetails() {
+    const { basket } = useAppSelector(state=>state.basket);
+    const dispatch = useAppDispatch();
     const { id } = useParams<{ id: string }>();
     const [product, setProduct] = useState<Product | null>();
     const [loading, setLoading] = useState(true);
+    const [quantity, setQuantity] = useState(0);
+    const [submitting, setSubmitting] = useState(false);
+    const item = basket?.items.find(i=> i.id === product?.id);
 
     const extractImageName = (item: Product): string | null => {
         if (item && item.pictureUrl) {
@@ -47,6 +54,42 @@ export default function ProductDetails() {
             .finally(() => setLoading(false));
     }, [id]);
 
+    const inputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = parseInt(event.target.value);
+        if (!isNaN(value) && value > 0) {
+            setQuantity(value);
+        }
+    };
+
+    const updateQuantity = async () => {
+        try {
+            setSubmitting(true);
+            const newItem = {
+                ...product!,
+                quantity: quantity
+            };
+            if (item) {
+                const quantityDifference = quantity - item.quantity;
+                if (quantityDifference > 0) {
+                    // Increment the quantity of an existing item in the basket
+                    await agent.Basket.incrementItemQuantity(item.id, quantityDifference, dispatch);
+                } else if (quantityDifference < 0) {
+                    // Decrement the quantity of an existing item in the basket
+                    await agent.Basket.decrementItemQuantity(item.id, Math.abs(quantityDifference), dispatch);
+                }
+            } else {
+                // Add a new item to the basket
+                await agent.Basket.addItem(newItem, dispatch);
+            }
+            setSubmitting(false);
+        } catch (error) {
+            console.error("Failed to update quantity:", error);
+            // Handle error
+            setSubmitting(false);
+        }
+    };
+
+
     if (loading) return <Spinner message="Loading Product..." /> //<h3>Loading...</h3>;
     if (!product) return <NotFound/>;
 
@@ -81,6 +124,31 @@ export default function ProductDetails() {
                         </TableBody>
                     </Table>
                 </TableContainer>
+                <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                        <TextField
+                            onChange={inputChange} 
+                            variant='outlined'
+                            type='number'
+                            label='Quantity in Cart'
+                            fullWidth
+                            value={quantity}
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <LoadingButton
+                            sx={{ height: '55px' }}
+                            color='primary'
+                            size='large'
+                            variant='contained'
+                            fullWidth
+                            loading={submitting}
+                            onClick={updateQuantity}
+                        >
+                            {item ? 'Update Quantity' : 'Add to Cart'}
+                        </LoadingButton>
+                    </Grid>
+                </Grid>
             </Grid>
         </Grid>
     );
